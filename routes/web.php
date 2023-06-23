1307951671d926b01720449f7106776a93fd796a
mizaeldouglas <mizaeldouglas2@gmail.com>
2023-06-23 14:12:00 -0300

    referenciar Rota no front

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProdutosController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
+});
+Route::prefix("/produtos")->group(function () {
+    Route::get('/', [ProdutosController::class,'index'])->name('produtos.index');
+    Route::get('/produtos/{id}', [ProdutosController::class,'show'])->name('produtos.show');
+    Route::post('/produtos', [ProdutosController::class,'store'])->name('produtos.store');
+    Route::put('/produtos/{id}', [ProdutosController::class,'update'])->name('produtos.update');
+    Route::delete('/produtos/{id}', [ProdutosController::class,'destroy'])->name('produtos.destroy');
 });
